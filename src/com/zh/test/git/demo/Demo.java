2a967ef105d8919acef550705dd330c8998d94fd
853827276 <853827276>
2018-12-26 11:15:42 +0800

    Revert "第五次提交"
    This reverts commit 0c7de6213bfb44133210bdb8a57bc30936ecce92.

--- src/com/zh/test/git/demo/Demo.java
+++ src/com/zh/test/git/demo/Demo.java
@@ -5,6 +5,5 @@ public class Demo {
 	public static void main(String[] args) {
 		System.out.println("-----------------");
 		System.out.println("-----------------");
-		System.out.println("----------5------");
 	}
 }
